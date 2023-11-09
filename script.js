f4a77cf11a999e7322a74d06b053e58f668dcdd6
Jonasi-Deetens <jonasideetens@hotmail.com>
2023-11-09 16:32:57 +0100

    Create cards for each letter

--- script.js
+++ script.js
@@ -1,4 +1,34 @@
-import { seiOn } from "./seion.js";
-import { dakuOn } from "./dakuon.js";
-import { yoOn } from "./yoon.js";
+import { seiOn } from "./collections/seion.js";
+import { dakuOn } from "./collections/dakuon.js";
+import { yoOn } from "./collections/yoon.js";
 
+createCards();
+
+function createCards() {
+    seiOn.forEach( seionChar => {
+        createCardElement(seionChar, "seion");
+    })
+
+    dakuOn.forEach( dakuonChar => {
+        createCardElement(dakuonChar, "dakuon");
+    })
+
+    yoOn.forEach( yoonChar => {
+        createCardElement(yoonChar, "yoon");
+    })
+}
+
+function createCardElement(value, section) {
+    let id = "#" + section;
+    console.log(id);
+    const cardSection = document.querySelector(id);
+
+    const card = document.createElement("section");
+    card.classList.add("card");
+
+    const h2 = document.createElement("p");
+    h2.textContent = value.letter;
+    card.append(h2);
+
+    cardSection.appendChild(card);
+};
